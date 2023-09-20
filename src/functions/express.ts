569deab49c9229a1dab12b53028ceceb04c8200e
adi_random <me.adi.hf@gmail.com>
2023-09-20 18:09:35 +0300

    add  netlify

--- src/functions/express.ts
+++ src/functions/express.ts
@@ -13,10 +13,11 @@ import {
 } from "@builder.io/qwik-city/middleware/node";
 import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
-import render from "./entry.ssr";
+import render from "../entry.ssr";
 import express from "express";
 import { fileURLToPath } from "node:url";
 import { join } from "node:path";
+import serverless from "serverless-http";
 
 declare global {
   interface QwikCityPlatform extends PlatformNode {}
@@ -25,9 +26,6 @@ declare global {
 // Directories where the static assets are located
 const distDir = join(fileURLToPath(import.meta.url), "..", "..", "dist");
 const buildDir = join(distDir, "build");
-
-// Allow for dynamic port
-const PORT = process.env.PORT ?? 3000;
 
 // Create the Qwik City Node middleware
 const { router, notFound } = createQwikCity({
@@ -62,8 +60,5 @@ app.use(router);
 // Use Qwik City's 404 handler
 app.use(notFound);
 
-// Start the express server
-app.listen(PORT, () => {
-  /* eslint-disable */
-  console.log(`Server started: http://localhost:${PORT}/`);
-});
+
+export const handler = serverless(app);
