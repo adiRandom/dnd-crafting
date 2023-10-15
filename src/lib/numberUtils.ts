ea146bb60a21d2742f006f5f1afbfb700ef54212
adi_random <me.adi.hf@gmail.com>
2023-10-15 19:05:49 +0300

    bugfixes

--- src/lib/numberUtils.ts
+++ src/lib/numberUtils.ts
@@ -1,3 +1,5 @@
+const MAX_32_BIT_INT = 2147483647;
+
 export function getRandomInt() {
-  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+  return Math.floor(Math.random() * MAX_32_BIT_INT);
 }
